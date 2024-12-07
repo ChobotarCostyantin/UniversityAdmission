17da4075987be8b6b79f713dab90207b75feb1bb
ChobotarCostyantin <chobotar.kostiantyn@chnu.edu.ua>
2024-12-07 12:25:29 +0200

    end

--- Program.cs
+++ Program.cs
@@ -100,6 +100,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-DBInitializer.Initialize();
+// DBInitializer.Initialize();
 
 app.Run();
